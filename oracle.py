b287cab9687a1f0537e2a6c20e7be884f2ff90a1
Alec Gunny <agunny@nvidia.com>
2020-10-19 10:23:58 -0700

    formatting fixes

--- oracle.py
+++ oracle.py
@@ -1,10 +1,13 @@
 import numpy as np
+
+
 np.random.seed(101588)
 
 a = 2
 b = 4
 c = 1
 sigma = 4
+
 
 def true_function(x):
     return a*x**2 + b*x + c
@@ -14,3 +17,4 @@ def sample(N):
     x = np.random.randn(N)
     y = true_function(x) + sigma*np.random.randn(N)
     return x, y
+
